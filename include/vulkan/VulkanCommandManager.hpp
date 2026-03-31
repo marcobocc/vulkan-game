23509d663f1ab454b7c982090dcf2c47f5267d21
marcobocc <marcobocchetti3@gmail.com>
2026-03-31 16:14:28 +0200

    [Vulkan Base Refactoring] Add VulkanCommandManager

--- include/vulkan/VulkanCommandManager.hpp
+++ include/vulkan/VulkanCommandManager.hpp
@@ -1,104 +1,125 @@
 #pragma once
 
-#include <log4cxx/logger.h>
 #include <vector>
 #include <volk.h>
 #include <vulkan/vulkan.h>
-#include "vulkan/VulkanDevice.hpp"
+#include "vulkan/VulkanErrorHandling.hpp"
 
 class VulkanCommandManager {
 public:
-    explicit VulkanCommandManager(VulkanDevice &device) : device_(device) {}
-    ~VulkanCommandManager() {
-        for (auto &[commandPool, frameFence]: frames_) {
-            if (frameFence) {
-                vkWaitForFences(device_.device(), 1, &frameFence, VK_TRUE, UINT64_MAX);
-                vkDestroyFence(device_.device(), frameFence, nullptr);
-            }
-            if (commandPool) {
-                vkDestroyCommandPool(device_.device(), commandPool, nullptr);
-            }
-        }
-    }
-    VulkanCommandManager(const VulkanCommandManager &) = delete;
-    VulkanCommandManager &operator=(const VulkanCommandManager &) = delete;
-    VulkanCommandManager(VulkanCommandManager &&) = delete;
-    VulkanCommandManager &operator=(VulkanCommandManager &&) = delete;
+    VulkanCommandManager(const VulkanCommandManager&) = delete;
+    VulkanCommandManager& operator=(const VulkanCommandManager&) = delete;
+    VulkanCommandManager(VulkanCommandManager&&) = delete;
+    VulkanCommandManager& operator=(VulkanCommandManager&&) = delete;
 
-    bool init() {
-        frames_.resize(MAX_FRAMES_IN_FLIGHT);
-        for (auto &[commandPool, frameFence]: frames_) {
-            VkCommandPoolCreateInfo poolInfo{};
-            poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-            poolInfo.queueFamilyIndex = device_.graphicsQueueFamilyIndex();
-            poolInfo.flags = 0;
-            if (vkCreateCommandPool(device_.device(), &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
-                return false;
+    explicit VulkanCommandManager(VkDevice device, uint32_t graphicsQueueFamilyIndex, VkQueue graphicsQueue) :
+        device_(device),
+        graphicsQueueFamilyIndex_(graphicsQueueFamilyIndex),
+        graphicsQueue_(graphicsQueue),
+        frames_(MAX_FRAMES_IN_FLIGHT) {
+        createCommandPools();
+        createFences();
+    }
+
+    ~VulkanCommandManager() {
+        for (auto& [commandPool, frameFence]: frames_) {
+            if (frameFence != VK_NULL_HANDLE) {
+                vkWaitForFences(device_, 1, &frameFence, VK_TRUE, UINT64_MAX);
+                vkDestroyFence(device_, frameFence, nullptr);
             }
-            VkFenceCreateInfo fenceInfo{};
-            fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-            fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
-            if (vkCreateFence(device_.device(), &fenceInfo, nullptr, &frameFence) != VK_SUCCESS) {
-                return false;
+            if (commandPool != VK_NULL_HANDLE) {
+                vkDestroyCommandPool(device_, commandPool, nullptr);
             }
         }
-        return true;
     }
-    uint32_t currentFrameIndex() const { return currentFrame_; }
+
     void beginFrame() {
-        auto &[commandPool, frameFence] = frames_.at(currentFrame_);
-        vkWaitForFences(device_.device(), 1, &frameFence, VK_TRUE, UINT64_MAX);
-        vkResetFences(device_.device(), 1, &frameFence);
-        vkResetCommandPool(device_.device(), commandPool, 0);
+        auto& [commandPool, frameFence] = frames_.at(currentFrame_);
+        vkWaitForFences(device_, 1, &frameFence, VK_TRUE, UINT64_MAX);
+        vkResetFences(device_, 1, &frameFence);
+        vkResetCommandPool(device_, commandPool, 0);
     }
+
     void endFrame() { currentFrame_ = (currentFrame_ + 1) % MAX_FRAMES_IN_FLIGHT; }
+
     VkCommandBuffer allocateCommandBuffer(VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY) {
-        auto &[commandPool, frameFence] = frames_.at(currentFrame_);
+        auto& [commandPool, frameFence] = frames_.at(currentFrame_);
         VkCommandBufferAllocateInfo alloc{};
         alloc.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
         alloc.commandPool = commandPool;
         alloc.level = level;
         alloc.commandBufferCount = 1;
-        VkCommandBuffer cmd = nullptr;
-        vkAllocateCommandBuffers(device_.device(), &alloc, &cmd);
+
+        VkCommandBuffer cmd = VK_NULL_HANDLE;
+        throwIfUnsuccessful(vkAllocateCommandBuffers(device_, &alloc, &cmd));
         return cmd;
     }
-    void submitCommandBuffer(VkCommandBuffer cmd, VkSemaphore waitSemaphore = VK_NULL_HANDLE,
+
+    void submitCommandBuffer(VkCommandBuffer cmd,
+                             VkSemaphore waitSemaphore = VK_NULL_HANDLE,
                              VkSemaphore signalSemaphore = VK_NULL_HANDLE) {
-        auto &[commandPool, frameFence] = frames_.at(currentFrame_);
+        auto& [commandPool, frameFence] = frames_.at(currentFrame_);
         VkSubmitInfo submit{};
         submit.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-        constexpr VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-        if (waitSemaphore) {
+
+        std::array<VkPipelineStageFlags, 1> waitStages = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+        if (waitSemaphore != VK_NULL_HANDLE) {
             submit.waitSemaphoreCount = 1;
             submit.pWaitSemaphores = &waitSemaphore;
-            submit.pWaitDstStageMask = waitStages;
+            submit.pWaitDstStageMask = waitStages.data();
         }
-        if (signalSemaphore) {
+
+        if (signalSemaphore != VK_NULL_HANDLE) {
             submit.signalSemaphoreCount = 1;
             submit.pSignalSemaphores = &signalSemaphore;
         }
+
         submit.commandBufferCount = 1;
         submit.pCommandBuffers = &cmd;
-        vkQueueSubmit(device_.graphicsQueue(), 1, &submit, frameFence);
+
+        throwIfUnsuccessful(vkQueueSubmit(graphicsQueue_, 1, &submit, frameFence));
     }
+
     static void beginCommandBuffer(VkCommandBuffer cmd,
                                    VkCommandBufferUsageFlags flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT) {
         VkCommandBufferBeginInfo begin{};
         begin.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
         begin.flags = flags;
-        vkBeginCommandBuffer(cmd, &begin);
+        throwIfUnsuccessful(vkBeginCommandBuffer(cmd, &begin));
     }
-    static void endCommandBuffer(VkCommandBuffer cmd) { vkEndCommandBuffer(cmd); }
+
+    static void endCommandBuffer(VkCommandBuffer cmd) { throwIfUnsuccessful(vkEndCommandBuffer(cmd)); }
 
 private:
+    void createCommandPools() {
+        for (auto& [commandPool, frameFence]: frames_) {
+            VkCommandPoolCreateInfo poolInfo{};
+            poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+            poolInfo.queueFamilyIndex = graphicsQueueFamilyIndex_;
+            poolInfo.flags = 0;
+            throwIfUnsuccessful(vkCreateCommandPool(device_, &poolInfo, nullptr, &commandPool));
+        }
+    }
+
+    void createFences() {
+        for (auto& [commandPool, frameFence]: frames_) {
+            VkFenceCreateInfo fenceInfo{};
+            fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+            fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+            throwIfUnsuccessful(vkCreateFence(device_, &fenceInfo, nullptr, &frameFence));
+        }
+    }
+
     static constexpr uint32_t MAX_FRAMES_IN_FLIGHT = 3;
+
     struct FrameContext {
         VkCommandPool commandPool = VK_NULL_HANDLE;
         VkFence frameFence = VK_NULL_HANDLE;
     };
-    VulkanDevice &device_;
+
+    VkDevice device_;
+    uint32_t graphicsQueueFamilyIndex_;
+    VkQueue graphicsQueue_;
     std::vector<FrameContext> frames_;
     uint32_t currentFrame_ = 0;
-    inline static log4cxx::LoggerPtr logger_ = log4cxx::Logger::getLogger("VulkanCommandManager");
 };
