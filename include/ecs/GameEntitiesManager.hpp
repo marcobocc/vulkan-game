4f7dfbbafce9a63f175ceabfd0c107b9a62ad152
marcobocc <marcobocchetti3@gmail.com>
2026-04-02 10:18:57 +0200

    [ECS Framework] Instantiate GameEntitiesManager template with available components

--- include/ecs/GameEntitiesManager.hpp
+++ include/ecs/GameEntitiesManager.hpp
@@ -5,88 +5,94 @@
 #include <unordered_map>
 #include <vector>
 
-template<typename... Components>
-class GameEntitiesManager {
-public:
-    using EntityId = uint64_t;
-    static constexpr size_t MaxComponents = 64;
-    using ComponentMask = std::bitset<MaxComponents>;
+namespace detail {
+    template<typename... Components>
+    class GameEntitiesManager {
+    public:
+        using EntityId = uint64_t;
+        static constexpr size_t MaxComponents = 64;
+        using ComponentMask = std::bitset<MaxComponents>;
 
-    EntityId createEntity() {
-        EntityId id = nextEntityId_++;
-        entityMasks_[id] = {};
-        entities_.push_back(id);
-        return id;
-    }
-
-    void destroyEntity(EntityId entity) {
-        removeAllComponents(entity);
-        entityMasks_.erase(entity);
-        auto it = std::ranges::find(entities_, entity);
-        if (it != entities_.end()) entities_.erase(it);
-    }
-
-    template<typename Component>
-    void addComponent(EntityId entity, const Component& component) {
-        getComponentMap<Component>()[entity] = component;
-        entityMasks_[entity].set(getComponentTypeId<Component>());
-    }
-
-    template<typename Component>
-    void removeComponent(EntityId entity) {
-        getComponentMap<Component>().erase(entity);
-        entityMasks_[entity].reset(getComponentTypeId<Component>());
-    }
-
-    template<typename Component>
-    Component& getComponent(EntityId entity) {
-        return getComponentMap<Component>().at(entity);
-    }
-
-    template<typename Component>
-    bool hasComponent(EntityId entity) const {
-        auto it = entityMasks_.find(entity);
-        return it != entityMasks_.end() && it->second.test(getComponentTypeId<Component>());
-    }
-
-    template<typename... QueryComponents>
-    auto query() {
-        using Result = std::tuple<EntityId, QueryComponents&...>;
-        std::vector<Result> results;
-        ComponentMask mask;
-        (mask.set(getComponentTypeId<QueryComponents>()), ...);
-        for (EntityId entity: entities_) {
-            auto it = entityMasks_.find(entity);
-            if (it == entityMasks_.end()) continue;
-            if ((it->second & mask) == mask) {
-                results.emplace_back(entity, getComponentMap<QueryComponents>().at(entity)...);
-            }
+        EntityId createEntity() {
+            EntityId id = nextEntityId_++;
+            entityMasks_[id] = {};
+            entities_.push_back(id);
+            return id;
         }
-        return results;
-    }
 
-private:
-    void removeAllComponents(EntityId entity) { (getComponentMap<Components>().erase(entity), ...); }
+        void destroyEntity(EntityId entity) {
+            removeAllComponents(entity);
+            entityMasks_.erase(entity);
+            auto it = std::ranges::find(entities_, entity);
+            if (it != entities_.end()) entities_.erase(it);
+        }
 
-    template<typename Component>
-    std::unordered_map<EntityId, Component>& getComponentMap() {
-        return std::get<std::unordered_map<EntityId, Component>>(componentMaps_);
-    }
+        template<typename Component>
+        void addComponent(EntityId entity, const Component& component) {
+            getComponentMap<Component>()[entity] = component;
+            entityMasks_[entity].set(getComponentTypeId<Component>());
+        }
 
-    template<typename Component>
-    const std::unordered_map<EntityId, Component>& getComponentMap() const {
-        return std::get<std::unordered_map<EntityId, Component>>(componentMaps_);
-    }
+        template<typename Component>
+        void removeComponent(EntityId entity) {
+            getComponentMap<Component>().erase(entity);
+            entityMasks_[entity].reset(getComponentTypeId<Component>());
+        }
 
-    template<typename>
-    static size_t getComponentTypeId() {
-        static size_t id = nextComponentTypeId_++;
-        return id;
-    }
+        template<typename Component>
+        Component& getComponent(EntityId entity) {
+            return getComponentMap<Component>().at(entity);
+        }
 
-    static inline size_t nextComponentTypeId_ = 0;
-    EntityId nextEntityId_ = 0;
-    std::vector<EntityId> entities_;
-    std::unordered_map<EntityId, ComponentMask> entityMasks_;
-    std::tuple<std::unordered_map<EntityId, Components>...> componentMaps_;
-};
+        template<typename Component>
+        bool hasComponent(EntityId entity) const {
+            auto it = entityMasks_.find(entity);
+            return it != entityMasks_.end() && it->second.test(getComponentTypeId<Component>());
+        }
+
+        template<typename... QueryComponents>
+        auto query() {
+            using Result = std::tuple<EntityId, QueryComponents&...>;
+            std::vector<Result> results;
+            ComponentMask mask;
+            (mask.set(getComponentTypeId<QueryComponents>()), ...);
+            for (EntityId entity: entities_) {
+                auto it = entityMasks_.find(entity);
+                if (it == entityMasks_.end()) continue;
+                if ((it->second & mask) == mask) {
+                    results.emplace_back(entity, getComponentMap<QueryComponents>().at(entity)...);
+                }
+            }
+            return results;
+        }
+
+    private:
+        void removeAllComponents(EntityId entity) { (getComponentMap<Components>().erase(entity), ...); }
+
+        template<typename Component>
+        std::unordered_map<EntityId, Component>& getComponentMap() {
+            return std::get<std::unordered_map<EntityId, Component>>(componentMaps_);
+        }
+
+        template<typename Component>
+        const std::unordered_map<EntityId, Component>& getComponentMap() const {
+            return std::get<std::unordered_map<EntityId, Component>>(componentMaps_);
+        }
+
+        template<typename>
+        static size_t getComponentTypeId() {
+            static size_t id = nextComponentTypeId_++;
+            return id;
+        }
+
+        static inline size_t nextComponentTypeId_ = 0;
+        EntityId nextEntityId_ = 0;
+        std::vector<EntityId> entities_;
+        std::unordered_map<EntityId, ComponentMask> entityMasks_;
+        std::tuple<std::unordered_map<EntityId, Components>...> componentMaps_;
+    };
+} // namespace detail
+
+#include "ecs/components/MaterialComponent.hpp"
+#include "ecs/components/MeshComponent.hpp"
+using GameEntitiesManager = detail::GameEntitiesManager<MeshComponent, MaterialComponent>;
